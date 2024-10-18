b931a5cfa5f346770be62a63dd457e9dc60bbbef
Igor Shovkun <ishovkun@slb.com>
2024-10-18 12:29:08 -0700

    price now uses a single i64

--- Price.hpp
+++ Price.hpp
@@ -1,40 +1,62 @@
 #pragma once
 #include <cstdint>
 #include <stdexcept>
+#include <string>
 
 // (7.5 format means up to 7 digits before the decimal
 // and exactly 5 digits after the decimal)
 class Price {
  private:
-  int32_t _integral;
-  int32_t _decimal;
+  int64_t _val;
 
   constexpr static int32_t MAX_INTEGRAL = 100'000'000;  // 10^8
   constexpr static int32_t MAX_DECIMAL = 1'000'000;        // 10^6
  public:
-  Price() : _integral(0), _decimal(0) {}
+  Price() : _val(0) {}
 
-  explicit Price(int32_t integral, int32_t decimal)
-      : _integral(integral), _decimal(decimal)
+  explicit Price(int64_t val) : _val(val)
   {
-    check_overflow(integral, decimal);
+    checkOverflow_(val);
   }
 
-  Price & operator+(Price const & other) {
-    _integral += other._integral;
-    auto dec = _decimal + other._decimal;
-    _integral += dec / MAX_DECIMAL;
-    _decimal = dec % MAX_DECIMAL;
-    check_overflow(_integral, _decimal);
-    return *this;
+  Price(std::string_view s) {
+    auto dot = s.find('.');
+    if (dot == std::string_view::npos) {
+      throw std::invalid_argument("No decimal point in the price");
+    }
+    if (dot > 7) {
+      throw std::invalid_argument("Integral part of the price is too big");
+    }
+    if (s.size() - dot - 1 != 5) {
+      throw std::invalid_argument("Decimal part of the price is too big");
+    }
+
+    auto integral = std::stoll(std::string(s.substr(0, dot)));
+    auto decimal = std::stoll(std::string(s.substr(dot + 1)));
+
+    if (integral < 0 || decimal < 0) {
+      throw std::invalid_argument("Negative price");
+    }
+
+    if (decimal >= MAX_DECIMAL) {
+      throw std::invalid_argument("Decimal part of the price is too big");
+    }
+
+    if (integral >= MAX_INTEGRAL) {
+      throw std::invalid_argument("Integral part of the price is too big");
+    }
+
+    _val = integral * MAX_DECIMAL + decimal;
   }
+
+  friend auto operator+(Price const & lhs, Price const & rhs) -> Price;
 
   auto operator==(Price const & other) const -> bool  {
-    return _integral == other._integral && _decimal == other._decimal;
+    return _val == other._val;
   }
 
   auto operator<(Price const & other) const -> bool  {
-    return _integral < other._integral || (_integral == other._integral && _decimal < other._decimal);
+    return _val < other._val;
   }
 
   auto operator!=(Price const & other) const -> bool  {
@@ -42,21 +64,22 @@ class Price {
   }
 
   friend std::ostream& operator<<(std::ostream& os, const Price& price);
+
 private:
-  void check_overflow(int32_t integral, int32_t decimal) {
-    if (decimal >= MAX_DECIMAL) {
+  auto checkOverflow_(int64_t val) const ->void {
+    if (val / MAX_DECIMAL >= MAX_INTEGRAL) {
       throw std::invalid_argument("Decimal part of the price is too big");
-    }
-    if (decimal < 0) {
-      throw std::invalid_argument("Decimal part of the price is negative");
-    }
-    if (integral >= MAX_INTEGRAL) {
-      throw std::invalid_argument("Price higher than the limit");
     }
   }
 };
 
+auto operator+(Price const & lhs, Price const & rhs) -> Price {
+  auto ans = Price(lhs._val + rhs._val);
+  ans.checkOverflow_(ans._val);
+  return ans;
+}
+
 std::ostream& operator<<(std::ostream& os, const Price& price) {
-  os << price._integral << "." << price._decimal;
+  os << price._val / Price::MAX_DECIMAL << "." << price._val % Price::MAX_DECIMAL;
   return os;
 }
--- tests/run_tests.cpp
+++ tests/run_tests.cpp
@@ -2,31 +2,21 @@
 #include "../Price.hpp"
 
 auto test_price() -> bool {
-  Price p1(100, 00000);
-  Price p2(100, 0);
+  Price p1("100.00000");
+  Price p2("100.00000");
   auto p3 = p1 + p2;
-  if (p3 != Price(200, 00000)) {
-    std::cout << p3 << " != "<< Price(200, 00000) << std::endl;
+  if (p3 != Price("200.00000")) {
+    std::cout << p3 << " != "<< Price("200.00000") << std::endl;
     return false;
   }
   // test overflow
   try {
-    auto p4 = p1 + Price((int32_t)200'000'000, 1);
+    auto p4 = p1 + Price("10'000'000.00");
     std::cout << "Should have thrown p4 = " << p4 << std::endl;
-    auto p5 = p1 + Price((int32_t)0, 1'000'001);
+    auto p5 = p1 + Price("0.1000001");
     std::cout << "Should have thrown p5 = " << p5 << std::endl;
     return false;
   } catch (std::invalid_argument const & e) {
-    // expected
-  }
-  // test sum overflow
-  auto p4 = Price(1'00'000'000-50, 0);
-  try {
-    auto p5 = p3 + p4;
-    std::cout << "Should have thrown p5 = " << p5 << std::endl;
-    return false;
-  }
-  catch (std::invalid_argument const & e) {
     // expected
   }
   return true;
@@ -45,6 +35,11 @@ void run_test(F f, std::string const & name) {
 auto main(int , char *[]) -> int {
 
   run_test(test_price, "Price");
+
+  std::cout << std::numeric_limits<uint64_t>::max() << std::endl;
+  std::cout << std::numeric_limits<int64_t>::max() << std::endl;
+  std::cout << std::numeric_limits<int64_t>::max() / double(1e12) << std::endl;
+  std::cout << std::numeric_limits<int64_t>::max() / double(1e12) << std::endl;
 
 
   return 0;
